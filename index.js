25a2c85e7f30911454f6cac6b89034a5f6ceb7b8
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-07-05 23:28:52 +0700

    Post to IFTTT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fetch = require('isomorphic-fetch')
 const cheerio = require('cheerio')
+const fs = require('fs')
 
-const data = {}
+const data = JSON.parse(fs.readFileSync('oneplus5-data.txt', 'utf8'))
+let isValueUpdated = false
 
 const fetchOnePlus5Price = async () => {
   const response = await fetch('http://www.aobmobile.net/wc/')
@@ -9,17 +11,37 @@ const fetchOnePlus5Price = async () => {
   const $ = cheerio.load(DOMString)
   const $onePlus = $('span').filter((idx, el) => $(el).text() === 'OnePlus 5')
   const $parent = $onePlus.parents('p')
-  const $onePlus5Line = $parent.find('span').filter((idx, el) => $(el).text().match(/^oneplus.?5.+gb.+\d{2},\d{3}.*\n$/i))
+  const $onePlus5Line = $parent.find('span').filter((idx, el) => $(el).text().match(/^oneplus.?5.+gb.+\d{2},\d{3}.*\n?$/i))
   const onePlusArray = []
   $onePlus5Line.each((idx, el) => onePlusArray.push($(el).text()))
   const results = onePlusArray.map(toPhoneObject)
   results.forEach(result => {
     const [variant, price] = result
-    data[variant] = +price.replace(',', '')
+    const cleanedVariant = variant.split(' ').map(a => a.trim()).filter(a => a).join(' ').slice(0, -1)
+    const priceNumber = +price.replace(',', '')
+    if (priceNumber < data[cleanedVariant]) {
+      data[cleanedVariant] = +price.replace(',', '')
+      isValueUpdated = true
+    }
   })
 }
 
 const toPhoneObject = text => text.match(/(.*)(\d{2},\d{3})/).slice(1)
 
-fetchOnePlus5Price()
-  .then(() => console.log(data))
+const async = async () => {
+  await fetchOnePlus5Price()
+  if (isValueUpdated) {
+    fs.writeFileSync('oneplus5-data.txt', JSON.stringify(data),'utf8')
+    fetch(process.env.IFTTT_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        value1: data
+      })
+    })
+  }
+}
+
+async()
