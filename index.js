18c01fac64799f0793b2724e91546da9eb742b5c
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-07-10 01:12:22 +0700

    Extract oneplus 5 data fetcher from scheduled tasks

--- index.js
+++ index.js
@@ -1,64 +1,12 @@
 const fetch = require('isomorphic-fetch')
-const cheerio = require('cheerio')
-const fs = require('fs')
-const redis = require('redis')
-const REDIS_URL = process.env.REDIS_URL
-
-const client = redis.createClient({
-  url: REDIS_URL
-})
-
-const getData = () => {
-  return new Promise((resolve, reject) => {
-    client.get('data', (err, reply) => {
-      if (err) {
-        console.error('error', err)
-        reject(err)
-      }
-
-      const data = JSON.parse(reply.toString())
-      resolve(data)
-    })
-  })
-}
-
-let isValueUpdated = false
-
-const fetchOnePlus5Price = async () => {
-  const response = await fetch('http://www.aobmobile.net/wc/')
-  const DOMString = await response.text()
-  const $ = cheerio.load(DOMString)
-  const $onePlus = $('span').filter((idx, el) => $(el).text() === 'OnePlus 5')
-  const $parent = $onePlus.parents('p')
-  const $onePlus5Line = $parent.find('span').filter((idx, el) => $(el).text().match(/^oneplus.?5.+gb.+\d{2},\d{3}.*\n?$/i))
-  const onePlusArray = []
-  $onePlus5Line.each((idx, el) => onePlusArray.push($(el).text()))
-  const results = onePlusArray.map(toPhoneObject)
-
-  let data = await getData()
-  console.log('data is', data)
-  results.forEach(result => {
-    const [variant, price] = result
-    const cleanedVariant = variant.split(' ').map(a => a.trim()).filter(a => a).join(' ').slice(0, -1)
-    const priceNumber = +price.replace(',', '')
-    if (priceNumber < data[cleanedVariant] || data[cleanedVariant] == undefined) {
-      data[cleanedVariant] = +price.replace(',', '')
-      isValueUpdated = true
-    }
-  })
-
-  return data
-}
-
-const toPhoneObject = text => text.match(/(.*)(\d{2},\d{3})/).slice(1)
+const { fetchOnePlus5Price } = require('./oneplusPriceFetcher')
 
 const async = async () => {
-  const data = await fetchOnePlus5Price()
+  const { updated, data } = await fetchOnePlus5Price()
 
-  if (isValueUpdated) {
+  if (updated) {
     console.log('updating data file and posting to Line')
     console.log(data)
-    client.set('data', JSON.stringify(data))
     fetch(process.env.IFTTT_WEBHOOK_URL, {
       method: 'POST',
       headers: {
@@ -72,10 +20,3 @@ const async = async () => {
 }
 
 async()
-  .then(() => {
-    client.quit()
-  })
-  .catch(err => {
-    console.error('error', err)
-    client.quit()
-  })
